f4dce1236572eff59e8c63f87f2f59aa15a01829
Weena <138716691+Weena-Nunkoosing@users.noreply.github.com>
2023-09-11 16:45:27 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+const button = document.getElementById('myButton');
+button.addEventListener('click', function() {
+    // Your code here
+});
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Your form handling code here
+});
+
+
+let searchBtn = document.querySelector ('search-btn');
+let searchBar = document.querySelector ('search-btn');
+
+searchBtn.addEventListener('click',() =>{
+
+})
+
